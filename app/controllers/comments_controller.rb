177e8d2bb75aeb1ad7727975a5e0345e4ee2b404
Abdi Ali <abdirazak.ali.12@live.com>
2013-12-05 17:24:20 +0000

    working comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+
+def create
+    @photo = Photo.find(params[:photo_id])
+    @comment = @photo.comments.create(params[:comment].permit(:commenter, :body))
+    redirect_to photo_path(@photo)
+  end
+
 end
