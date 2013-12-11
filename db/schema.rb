06dfc0801fb0c79dacdcc84893a019bf6d510330
Abdi Ali <prodef@live.com>
2013-12-11 18:30:09 +0000

    created relationship table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210190745) do
+ActiveRecord::Schema.define(version: 20131211181622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20131210190745) do
   end
 
   add_index "comments", ["photo_id"], name: "index_comments_on_photo_id", using: :btree
+
+  create_table "favourite_photos", force: true do |t|
+    t.integer  "photo_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", force: true do |t|
     t.string   "title"
@@ -53,6 +60,14 @@ ActiveRecord::Schema.define(version: 20131210190745) do
     t.text     "home_town"
     t.datetime "date_of_birth"
     t.text     "gender"
+    t.string   "username"
+  end
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "tags", force: true do |t|
