04d6fb558f7ec8cc476070e35cac0278c552eaaf
Arturo Ortiz <elsushi@gmail.com>
2013-12-13 17:06:42 +0000

    added follow button unfunctional

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20131211181622) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +61,6 @@ ActiveRecord::Schema.define(version: 20131211181622) do
     t.datetime "date_of_birth"
     t.text     "gender"
     t.string   "username"
-
   end
 
   create_table "relationships", force: true do |t|
@@ -71,8 +68,11 @@ ActiveRecord::Schema.define(version: 20131211181622) do
     t.integer  "followed_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-
   end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "tags", force: true do |t|
     t.string   "name"
