1fb8d5c277c5bb8f6b2a861688d74d86aaf5f297
Abdi Ali <prodef@live.com>
2013-12-16 09:44:34 +0000

    users semi

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,12 @@
 class RelationshipsController < ApplicationController
 
- before_action :signed_in_user
+ before_action :authenticate_user!
 
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
     respond_to do |format|
-      format.html { redirect_to @user }
+      format.html { redirect_to @user.profile }
       format.js
     end
   end
@@ -15,7 +15,7 @@ class RelationshipsController < ApplicationController
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
     respond_to do |format|
-      format.html { redirect_to @user }
+      format.html { redirect_to @user.profile }
       format.js
     end
   end
