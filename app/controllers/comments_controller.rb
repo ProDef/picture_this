4909355142d055265bb801a2f8f07a27e2997138
Abdi Ali <prodef@live.com>
2013-12-18 19:27:04 +0000

    added remove comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
 
-def create
+  def create
     @photo = Photo.find(params[:photo_id])
     @comment = @photo.comments.create(params[:comment].permit(:user, :body))
+    redirect_to photo_path(@photo)
+  end
+
+  def destroy
+    @photo = Photo.find(params[:photo_id])
+    @comment = @photo.comments.find(params[:id])
+    @comment.destroy
     redirect_to photo_path(@photo)
   end
 
